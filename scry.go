cff88bf397db65cf4c0b2009eb61b31ef5a17fb0
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-03-10 19:02:06 -0700

    Readd authenticator. Still needed for new spotify client.

--- scry.go
+++ scry.go
@@ -1,2 +1,23 @@
 package scry
 
+import (
+	"github.com/Henry-Sarabia/blank"
+	"github.com/pkg/errors"
+	"github.com/zmb3/spotify"
+)
+
+func authenticator(URI string) (*spotify.Authenticator, error) {
+	if blank.Is(URI) {
+		return nil, errors.New("URI is blank")
+	}
+
+	auth := spotify.NewAuthenticator(
+		URI,
+		spotify.ScopePlaylistModifyPublic,
+		spotify.ScopeUserReadPrivate,
+		spotify.ScopeUserTopRead,
+		spotify.ScopeUserReadRecentlyPlayed,
+		)
+
+	return &auth, nil
+}
--- scry_test.go
+++ scry_test.go
@@ -1,2 +1,26 @@
 package scry
 
+import (
+	"github.com/pkg/errors"
+	"testing"
+)
+
+func TestAuthenticator(t *testing.T) {
+	tests := []struct {
+		name string
+		uri string
+		wantErr error
+	}{
+		{"Empty URI", "", errors.New("URI is blank")},
+		{"Non-empty URI", "some-uri", nil},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			_, err := authenticator(test.uri)
+			if err != test.wantErr {
+				t.Errorf("got: <%v>, want: <%v>", err, test.wantErr)
+			}
+		})
+	}
+}
+
