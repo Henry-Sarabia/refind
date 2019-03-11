cf71a9bc5f0734bbfd664c3be9360020d735157a
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-03-10 18:59:11 -0700

    User login is out of scope for this package

--- scry.go
+++ scry.go
@@ -1,23 +1,2 @@
 package scry
 
-import (
-	"github.com/Henry-Sarabia/blank"
-	"github.com/pkg/errors"
-	"github.com/zmb3/spotify"
-)
-
-func authenticator(URI string) (*spotify.Authenticator, error) {
-	if blank.Is(URI) {
-		return nil, errors.New("URI is blank")
-	}
-
-	auth := spotify.NewAuthenticator(
-		URI,
-		spotify.ScopePlaylistModifyPublic,
-		spotify.ScopeUserReadPrivate,
-		spotify.ScopeUserTopRead,
-		spotify.ScopeUserReadRecentlyPlayed,
-		)
-
-	return &auth, nil
-}
