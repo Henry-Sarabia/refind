e8e499c0987b390d04e03205e73cbfc021117c61
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-03-17 22:33:32 -0700

    Add Recommendations from Artists

--- scry.go
+++ scry.go
@@ -42,3 +42,27 @@ func (s *Scryer) FromTracks(name string) (*Playlist, error) {
 
 	return &pl, nil
 }
+
+func (s *Scryer) FromArtists(name string) (*Playlist, error) {
+	top, err := s.serv.TopArtists()
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot fetch top artists")
+	}
+
+	var sds []Seed
+	for _, t := range top {
+		sds = append(sds, t.Seed())
+	}
+
+	recs, err := s.serv.Recommendations(sds)
+	if err != nil {
+		return nil, errors.Wrap(err, "cannot fetch recommendations")
+	}
+
+	pl, err := s.serv.Playlist(name, recs)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot create playlist with name: %s", name)
+	}
+
+	return &pl, nil
+}
