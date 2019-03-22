1f869dcb4d5af437a94067bf727ed0f6cb17262e
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-03-22 16:14:11 -0700

    Remove deprecated seed code from SpotifyService

--- spotifyservice/seed.go
+++ spotifyservice/seed.go
@@ -59,23 +59,3 @@ func parseSeed(old scry.Seed, sd *spotify.Seeds) error {
 
 	return nil
 }
-
-//func Seeds(sdr scry.Seeder) (spotify.Seeds, error) {
-//	sds := sdr.Seeds()
-//	var spot spotify.Seeds
-//
-//	for _, sd := range sds {
-//		switch sd.Category {
-//		case scry.TrackSeed:
-//			spot.Tracks = append(spot.Tracks, spotify.ID(sd.ID))
-//		case scry.ArtistSeed:
-//			spot.Artists = append(spot.Artists, spotify.ID(sd.ID))
-//		case scry.GenreSeed:
-//			spot.Genres = append(spot.Genres, sd.Name)
-//		default:
-//			return spotify.Seeds{}, errors.New("unexpected Seed category")
-//		}
-//	}
-//
-//	return spot, nil
-//}
