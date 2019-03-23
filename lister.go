163a4d08cca7e2f31114812f1b8c213b34061fc4
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-03-23 14:56:25 -0700

    Change PopFilter to Lister and add Filter method

--- lister.go
+++ lister.go
@@ -2,9 +2,9 @@ package scry
 
 import "github.com/pkg/errors"
 
-type PopFilter struct {
+type Lister struct {
 	serv MusicService
-	rec Recommender
+	rec  Recommender
 }
 
 type MusicService interface {
@@ -17,8 +17,8 @@ type Recommender interface {
 	Recommendations([]Seed) ([]Track, error)
 }
 
-func (pf PopFilter) FromTracks(name string) ([]Track, error) {
-	tracks, err := pf.serv.RecentTracks()
+func (l Lister) FromTracks(name string) ([]Track, error) {
+	tracks, err := l.serv.RecentTracks()
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot fetch recent tracks")
 	}
@@ -32,12 +32,23 @@ func (pf PopFilter) FromTracks(name string) ([]Track, error) {
 		sds = append(sds, sd)
 	}
 
-	recs, err := pf.rec.Recommendations(sds)
+	recs, err := l.rec.Recommendations(sds)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot fetch recommendations")
 	}
 
 	return recs, nil
+}
+
+func (l Lister) Filter(prev []Track, rmv map[string]Artist) []Track {
+	var curr []Track
+	for _, p := range prev {
+		if _, ok := rmv[p.Artist.Name]; !ok {
+			curr = append(curr, p)
+		}
+	}
+
+	return curr
 }
 
 //func FromArtists(serv MusicService, rec Recommender, name string) (*Playlist, error) {
