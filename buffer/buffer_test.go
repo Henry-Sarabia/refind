3ca9c4aa6880e7f3ccbd7032cac038f7b76a93cd
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-03-29 21:00:12 -0700

    Add RecentTracks tests

--- buffer/buffer_test.go
+++ buffer/buffer_test.go
@@ -11,10 +11,17 @@ var (
 	testErrArtists = errors.New("cannot fetch artists")
 	testErrTracks = errors.New("cannot fetch tracks")
 )
+
 var testArtists = []refind.Artist{
 	{ID: "0", Name: "foo"},
 	{ID: "1", Name: "bar"},
 	{ID: "2", Name: "baz"},
+}
+
+var testTracks = []refind.Track{
+	{ID: "10", Name: "corge", Artist: refind.Artist{ID: "0", Name: "foo"}},
+	{ID: "11", Name: "grault", Artist: refind.Artist{ID: "1", Name: "bar"}},
+	{ID: "12" , Name: "garply", Artist: refind.Artist{ID: "1", Name: "bar"}},
 }
 
 type fakeMusicService struct {
@@ -179,6 +186,124 @@ func TestBuffer_TopArtists(t *testing.T) {
 
 			if !reflect.DeepEqual(got, test.wantArt) {
 				t.Errorf("got: <%v>, want: <%v>", got, test.wantArt)
+			}
+		})
+	}
+}
+
+func TestBuffer_RecentTracks(t *testing.T) {
+	tests := []struct {
+		name string
+		buf buffer
+		wantTracks []refind.Track
+		wantErr error
+	}{
+		{
+			name: "Valid response, nil buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: testTracks,
+					trackErr: nil,
+				},
+				tracks: nil,
+			},
+			wantTracks: testTracks,
+			wantErr: nil,
+		},
+		{
+			name: "Valid response, empty buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: testTracks,
+					trackErr: nil,
+				},
+				tracks: []refind.Track{},
+			},
+			wantTracks: testTracks,
+			wantErr: nil,
+		},
+		{
+			name: "Valid response, single track buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: testTracks,
+					trackErr: nil,
+				},
+				tracks: []refind.Track{{ID: "11", Name: "grault", Artist: refind.Artist{ID: "1", Name: "bar"}}},
+			},
+			wantTracks: []refind.Track{{ID: "11", Name: "grault", Artist: refind.Artist{ID: "1", Name: "bar"}}},
+			wantErr: nil,
+		},
+		{
+			name: "Valid response, multiple track buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: testTracks,
+					trackErr: nil,
+				},
+				tracks: testTracks,
+			},
+			wantTracks: testTracks,
+			wantErr: nil,
+		},
+		{
+			name: "Invalid response, nil buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: nil,
+					trackErr: testErrTracks,
+				},
+				tracks: nil,
+			},
+			wantTracks: nil,
+			wantErr: testErrTracks,
+		},
+		{
+			name: "Invalid response, empty buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: nil,
+					trackErr: testErrTracks,
+				},
+				tracks: []refind.Track{},
+			},
+			wantTracks: nil,
+			wantErr: testErrTracks,
+		},
+		{
+			name: "Invalid response, single track buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: nil,
+					trackErr: testErrTracks,
+				},
+				tracks: []refind.Track{{ID: "11", Name: "grault", Artist: refind.Artist{ID: "1", Name: "bar"}}},
+			},
+			wantTracks: []refind.Track{{ID: "11", Name: "grault", Artist: refind.Artist{ID: "1", Name: "bar"}}},
+			wantErr: nil,
+		},
+		{
+			name: "Invalid response, multiple track buffer",
+			buf: buffer{
+				serv: fakeMusicService{
+					tracks: nil,
+					trackErr: testErrTracks,
+				},
+				tracks: testTracks,
+			},
+			wantTracks: testTracks,
+			wantErr: nil,
+		},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, err := test.buf.RecentTracks()
+			if err != test.wantErr {
+				t.Errorf("got: <%v>, want: <%v>", err, test.wantErr)
+			}
+
+			if !reflect.DeepEqual(got, test.wantTracks) {
+				t.Errorf("got: <%v>, want: <%v>", got, test.wantTracks)
 			}
 		})
 	}
